1649b093f2a57669b6fa37c204a3b141f34ed00e
shubham777580 <shubhammukherjee370@gmail.com>
2022-08-01 16:10:21 +0530

    .net code
    commit by shubham

--- new1.cs
+++ new1.cs
@@ -1,5 +1,22 @@
-int Salary = 10;
-float ApproxSalary = 10.00;
-const int Salary = 10;
-const float ApproxSalary = 10.00;
+//int Salary = 10;
+//float ApproxSalary = 10.00;
+//const int Salary = 10;
+//const float ApproxSalary = 10.00;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+class Program 
+{
+    public static void Main (string[] args)
+    {
+        string msg = "Hello world";
+        Console.WriteLine(msg);
+    }
+}
+}
