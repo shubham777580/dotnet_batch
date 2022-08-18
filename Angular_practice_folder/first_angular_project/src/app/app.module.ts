8e2195f76d399b542258ea504cbb72cf03d395aa
shubham777580 <shubhammukherjee370@gmail.com>
2022-08-18 12:43:32 +0530

    angular practice

--- Angular_practice_folder/first_angular_project/src/app/app.module.ts
+++ Angular_practice_folder/first_angular_project/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MyAppComponent } from 'src/my-app/my-app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { App2Component } from 'src/app2/app2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyAppComponent
+    MyAppComponent,
+    App2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    //MyAppComponent
+    //impoMyAppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
