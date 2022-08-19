5d5f7ed217fb1fdeb98ad8d4b8c7eb03ae997158
shubham777580 <shubhammukherjee370@gmail.com>
2022-08-19 17:42:20 +0530

    react basic js learning

--- React-handson/reactproj2/src/index.js
+++ React-handson/reactproj2/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Car from './Car';
+import Engine from './Engine';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Engine />
+    <Car />
     <App />
   </React.StrictMode>
 );
