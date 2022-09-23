91a04f1a9ad77196c8bc8a8a4b57cf0cc7815081
shubham777580 <shubhammukherjee370@gmail.com>
2022-09-23 12:35:31 +0530

    selenium automation practice

---
+++ selenium_automation_console/selenium_automation_console/Guru99Demo.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guru99Demo
+{
+    class Guru99Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:/Users/shubham.mukherjee/Downloads/chromedriver_win32");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
---
+++ selenium_automation_console/selenium_automation_console/Program.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Automation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            //IWebDriver driver = new ChromeDriver("C:/Users/shubham.mukherjee/Downloads/chromedriver_win32");
+
+            //// This will open up the URL
+            //driver.Url = "https://www.geeksforgeeks.org/";
+
+            Guru99Demo guru99Demo = new Guru99Demo();
+            guru99Demo.startBrowser();
+            guru99Demo.test();
+            guru99Demo.closeBrowser();
+
+
+        }
+    }
+
+    class Guru99Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:/Users/shubham.mukherjee/Downloads/chromedriver_win32");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
